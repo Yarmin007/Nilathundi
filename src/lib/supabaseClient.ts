b84913fb533cc45a71eae245d8a9556c7428d71e
Yarmin007 <88593354+Yarmin007@users.noreply.github.com>
2026-01-14 00:30:35 +0600

    Fix supabase client crash

--- src/lib/supabaseClient.ts
+++ src/lib/supabaseClient.ts
@@ -1,6 +1,7 @@
 import { createBrowserClient } from '@supabase/ssr'
 
-const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL!
-const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
+// Change "!" to "|| ''"
+const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL || ''
+const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY || ''
 
 export const supabase = createBrowserClient(supabaseUrl, supabaseKey)
